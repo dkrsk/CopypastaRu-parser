aeef3f140d2a10bf847e8a89b11b6d1232c99254
Dn Krsk <wileps2310@yandex.ru>
2024-01-19 23:11:48 +0300

    oops

--- PastaParser/Library.fs
+++ PastaParser/Library.fs
@@ -52,7 +52,7 @@ module PastaParser =
         let node = html.DocumentNode.SelectNodes("//main/div/div").Item(0).ChildNodes[1].ChildNodes[1] // div класс с пастой
 
         let pasta = new Pasta(node.FirstChild.InnerText, // div класс с текстом пасты
-            Seq.toArray (seq { for i in node.ChildNodes[2].ChildNodes do i.ChildNodes[1].InnerText }), // div класс с тегами пасты
+            Seq.toArray (seq { for i in node.ChildNodes[2].ChildNodes do i.SelectNodes("./text()[normalize-space(.) != '']").Item(0).InnerText }), // div класс с тегами пасты
             pastaURL)
         // названия классов на сайте это вообще прикол)
         // админ сайта, я тебя очень люблю, спасибо за обфускацию в некомерческом проекте. Пожалуйста, не меняй html структуру...
